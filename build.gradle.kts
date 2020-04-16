bf162e084720e903eca2e9da414163c1af84aa45
hongsii <hongsii.dev@gmail.com>
2020-04-16 23:46:37 +0900

    Extract version constant

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+object Versions {
+    const val kotlin = "1.3.71"
+}
+
 plugins {
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.3.71"
-    kotlin("plugin.spring") version "1.3.71"
-    kotlin("plugin.jpa") version "1.3.71"
+    kotlin("jvm") version Versions.kotlin
+    kotlin("plugin.spring") version Versions.kotlin
+    kotlin("plugin.jpa") version Versions.kotlin
 }
 
 allprojects {
